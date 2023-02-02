2fd0244ff2bee40732fc77eb9221a48e210820a9
Richard Albert Leddy <richardaleddy@gmail.com>
2023-02-01 23:36:37 -0800

    Added in the caching version of the file ops
    Allows for configuration of a hash object.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 
 
 let FileOperations = require('./lib/file_ops')
-
+let FileOperationsCache = require('file_ops_cache')
 
 
 
 
 
 module.export.FileOperations = FileOperations
+module.exports.FileOperationsCache = FileOperationsCache

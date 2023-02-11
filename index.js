93913e24fcf60f910ded5da6521acc34ce861b25
Richard Albert Leddy <richardaleddy@gmail.com>
2023-02-11 15:52:35 -0800

    Add a class for loading and syncing a directory
    Add dir reader  add  a watch initializer

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
-
-
+//  extra-file-class
+// 
 let FileOperations = require('./lib/file_ops')
 let FileOperationsCache = require('./lib/file_ops_cache')
-
-
-
+let DirectoryCache = require('./lib/directory_caches')
 
 
 module.exports.FileOperations = FileOperations
 module.exports.FileOperationsCache = FileOperationsCache
+module.exports.DirectoryCache = DirectoryCache
